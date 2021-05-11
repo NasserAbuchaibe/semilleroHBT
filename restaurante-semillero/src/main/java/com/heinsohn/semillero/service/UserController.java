80209479f4ccb084e876430def1f560b3106e4d5
NasserAbuchaibe <nasser.abuchaibe@gmail.com>
2021-05-11 00:36:43 -0500

    Se agrega el DTO y el DAO a la aplicacion, se incorpora al archivo pom las dependencias para el jpa y el conector con la DB mysql

--- restaurante-semillero/src/main/java/com/heinsohn/semillero/service/UserController.java
+++ restaurante-semillero/src/main/java/com/heinsohn/semillero/service/UserController.java
@@ -1,5 +1,7 @@
 package com.heinsohn.semillero.service;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,47 +9,75 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.heinsohn.semillero.dto.UsuarioDTO;
+import com.heinsohn.semillero.repository.UsuariariosRepository;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("usuarios")
 public class UserController {
 
-	/**
-	 * Metodo que retorma datos del usurio
-	 * por medio del metodo http GET
-	 * @return String
-	 */
-	@GetMapping
-	public String getUsuario(String nombre) {
-		return "Usuario " +  nombre + " creado.";
+	private UsuariariosRepository uRepository;
+	
+	public UserController(UsuariariosRepository uRepository) {
+		this.uRepository = uRepository;
 	}
 	
 	/**
-	 * Metodo que crea un nuevo usuario
-	 * por medio del metodo http POST
+	 * Metodo que retorna los datos de los usuarios
+	 * por medio del metodo HTTP.GET
+	 * 
+	 * @return String
+	 */
+	@ApiOperation(value = "Metodo que retorna los datos de los usuarios por medio del metodo HTTP.GET",
+				  response = Integer.class, tags = "consultarUsuarios")
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "Exitoso, funcion!"),
+		@ApiResponse(code = 401, message = "No autorizado, pilas!"),
+		@ApiResponse(code = 403, message = "No tienes permiso!"),
+		@ApiResponse(code = 404, message = "No se encontro lo que buscabas!")
+	})
+	@GetMapping
+	public List<UsuarioDTO> getUsuario() {
+		return (List<UsuarioDTO>) uRepository.findAll();
+	}
+	
+	/**
+	 * Metodo que permite registrar los datos de un usuario
+	 * por medio del metodo HTTP.POST
+	 * 
 	 * @return String
 	 */
 	@PostMapping
-	public String crearUsuario() {
-		return "Usuario creado";
+	public String crearUsuario(UsuarioDTO usuario) {
+		return "El usuario se registro exitosamente!";
 	}
 	
 	/**
-	 * Metodo que actualiza un usuario
-	 * por medio del metodo http PUT
+	 * Metodo que permite actualizar los datos de un usuario
+	 * por medio del metodo HTTP.PUT
+	 * 
 	 * @return String
 	 */
 	@PutMapping
-	public String actualizarUsuario( ) {
-		return "Usuario actualizado";
+	public String actualizarUsuario(UsuarioDTO usuario) {
+		
+		return "Se actualizo el usuario!";
 	}
 	
 	/**
-	 * Metodo que elimina un usuario
-	 * por medio del metodo http DELETE
+	 * Este metodo permite eliminar la información
+	 * de un usuario especifico
+	 * 
 	 * @return String
 	 */
 	@DeleteMapping
-	public String deleteUsuario( ) {
-		return "Usuario borrado";
+	public String eliminarUsuario(int idUsuario) {
+		
+		return "Se elimino el usuario exitosament!";
 	}
+	
 }
